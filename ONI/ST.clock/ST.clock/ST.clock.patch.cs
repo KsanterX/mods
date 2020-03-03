d63736a022a476e60e1580bb2119fd11aedf4d80
Artem Semibolotnyy <ksanterx@gmail.com>
2020-03-03 20:05:42 +0300

    Revert "Revert "Version 1.0""
    This reverts commit 76353d412cb99ba70e0ad3e3e3829cd64ac95c23.

---
+++ ONI/ST.clock/ST.clock/ST.clock.patch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Harmony;
+
+namespace ST.clock
+{
+    class STclockPatch
+    {
+		[HarmonyPatch(typeof(MeterScreen), "RefreshRations")]
+		private static class Refresh_Patch
+		{
+			[HarmonyPostfix]
+			public static void RefreshPatch(MeterScreen __instance)
+			{
+				if (__instance.RationsText.text.Contains("|"))
+				{
+					string text = __instance.RationsText.text.Split(new char[] { '|' })[0];
+					__instance.RationsText.text = text.Trim() + " | " + STclockMod.FormatAsClock(STclockMod.GetClock());
+					return;
+				}
+				LocText rationsText = __instance.RationsText;
+				rationsText.text = rationsText.text + " | " + STclockMod.FormatAsClock(STclockMod.GetClock());
+				return;
+			}
+		}
+	}
+}
