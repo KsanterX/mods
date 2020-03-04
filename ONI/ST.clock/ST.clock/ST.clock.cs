54458a7d1a7834621f8c71a6dd3867baee5c2e2d
Artem Semibolotnyy <ksanterx@gmail.com>
2020-03-04 08:22:31 +0300

    System Clock Steam release

--- ONI/ST.clock/ST.clock/ST.clock.cs
+++ ONI/ST.clock/ST.clock/ST.clock.cs
@@ -6,10 +6,10 @@ namespace ST.clock
 	public class STclockMod
 	{
 		static Dictionary<string, string> DayTimeColors = new Dictionary<string, string>{
-			{ "Morning", "#ebb74" },
+			{ "Morning", "#eebb74" },
 			{ "Day", "#ef810e" },
 			{ "Evening", "#09679a" },
-			{ "Night", "#33c416" }
+			{ "Night", "#003a55" }
 		};
 		public static string GetClock()
 		{
@@ -19,7 +19,7 @@ namespace ST.clock
 		public static string FormatAsClock(string text)
 		{
 
-			return "<b><color=" + DayTimeColors[CheckTimeOfDay()] + "><b>" + text + "</color>";
+			return "<b><color=" + DayTimeColors[CheckTimeOfDay()] + ">" + text + "</color></b>";
 		}
 
 		public static string CheckTimeOfDay()
@@ -29,13 +29,16 @@ namespace ST.clock
 			if (time >= 5 && time < 10)
 			{
 				return "Morning";
-			} else if (time >= 10 && time < 17)
+			}
+			else if (time >= 10 && time < 17)
 			{
 				return "Day";
-			} else if (time >= 17 && time < 21)
+			}
+			else if (time >= 17 && time < 21)
 			{
 				return "Evening";
-			} else
+			}
+			else
 			{
 				return "Night";
 			}
